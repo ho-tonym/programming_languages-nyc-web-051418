56c64e724a58955415ec9261417fd77ccd091ef9
Tony Ho <ho.tonym@gmail.com>
2018-05-12 16:12:57 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -36,14 +36,15 @@ end
 def reformat_languages(languages)
  # your code here
   new_hash = Hash.new
-  languages.each do |paradigm, paradigm_languages|
-    paradigm_languages.each do |language, type_hash|
+  
+  languages.each do |paradigm, p_languages|
+    p_language.each do |language, type_hash|
       if !new_hash.keys.include?(language)
         styles = Array.new
         styles.push(paradigm)
         new_hash[language] = { :type => type_hash[:type], :style => styles }
       else
-        new_hash[language][:style].push(paradigm)
+        new_hash[language][:style] << paradigm
       end
     end
   end
